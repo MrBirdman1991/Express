187a0692ec30fa144c1c2aa71a7ac5ca2926f2b2
MrBirdman1991 <birdman-coding@outlook.de>
2022-06-19 20:59:01 +0200

    testing vitest

--- server/src/__tests__/product.test.ts
+++
@@ -1,9 +0,0 @@
-describe("product", () => {
-  describe("get product route", () => {
-    it("should return 404 when product not exists", async () => {
-      const productId = "123";
-
-      expect(true).toBe(true);
-    });
-  });
-});
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,16 @@
+import express from "express";
 import config from "config";
 import connect from "./utils/connect";
 import logger from "./utils/logger";
+import { deserializeUser } from "./middleware/deserializeUser";
+import routes from "./routes";
 
-import createServer from "./utils/server";
-
-const app = createServer();
+const app = express();
 const port = config.get<number>("port");
+
+app.use(express.json());
+app.use(deserializeUser);
+routes(app);
 
 app.listen(port, async () => {
   logger.info(`app is running at ${port}`);

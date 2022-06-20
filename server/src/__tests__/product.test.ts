265e359779273877957b2ebbf1c64aba17e21354
MrBirdman1991 <birdman-coding@outlook.de>
2022-06-20 11:05:39 +0200

    start testing

---
+++ server/src/__tests__/product.test.ts
@@ -0,0 +1,7 @@
+describe("product", () => {
+  describe("get product route", () => {
+    it("should return 404 if given Product not exists", async () => {
+      expect(true).toBe(true);
+    });
+  });
+});
